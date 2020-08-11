b3d48c4f9e71a2e23a9fa9f19387049b39596113
cicada <sasaleaf01@gmail.com>
2020-08-11 22:18:05 +0900

    rfct: 単語に応じた受け答え処理を関数化

--- res_greet.py
+++ res_greet.py
@@ -21,22 +21,7 @@ sock.connect((host, port))
 def play_voice(name):
     subprocess.call(cmd_fmt.format(name), shell=True)
 
-res = ''
-while True:
-    # 音声認識の区切り[\n.]まで受信
-    while (res.find('\n.') == -1):
-        res += sock.recv(1024).decode('utf-8')
-
-    # 認識文字列を抽出
-    word = ''
-    for line in res.split('\n'):
-        index = line.find('WORD=')
-        if index != -1:
-            line = line[index+6:line.find('"', index+6)]
-            if line != '[s]':
-                word = word + line
-
-    # 認識文字列にキーワードがあれば対応
+def speak_response(word):
     print("WORD:"+word)
     if word == 'おはよう[/s]':
         print("=>おはよう")
@@ -53,5 +38,23 @@ while True:
     if word == 'ただいま[/s]':
         print("=>おかえり")
         play_voice(random.choice(vlist_oka))
+
+res = ''
+while True:
+    # 音声認識の区切り[\n.]まで受信
+    while (res.find('\n.') == -1):
+        res += sock.recv(1024).decode('utf-8')
+
+    # 認識文字列を抽出
+    word = ''
+    for line in res.split('\n'):
+        index = line.find('WORD=')
+        if index != -1:
+            line = line[index+6:line.find('"', index+6)]
+            if line != '[s]':
+                word = word + line
+
+    # 認識文字列にキーワードがあれば対応
+    speak_response(word)
 
     res = ''
