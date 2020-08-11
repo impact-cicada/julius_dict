be9126d314e6be8001466257b4e4909916d4091f
cicada <sasaleaf01@gmail.com>
2020-08-11 23:58:38 +0900

    fix: julius実行引数変更、jconfファイルをgrammer-kitに変更

--- startup_greet.sh
+++ startup_greet.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-JCONF=~/julius/dictation-kit-4.5/am-gmm.jconf
+JCONF=~/julius/grammar-kit-4.3.1/hmm_mono.jconf
 DICT=~/julius/dict/greet
+LEVEL=5000
+RJSH=600
 
 echo JCONF=$JCONF
 echo DICT=$DICT
 
-CMD="julius -C $JCONF -nostrip -gram $DICT -input mic -module"
+CMD="julius -C $JCONF -nostrip -gram $DICT -lv $LEVEL -rejectshort $RJSH -input mic -module"
 echo $CMD
 $CMD
